8c197b4e7650cec0bc3a5cdb3b2c3ad66dc88764
Rick Love <rick@toldpro.com>
2022-08-01 12:31:37 -0500

    add bit field

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,3 +1,4 @@
 export * from './src/types';
 export * from './src/random';
 export * from './src/utils';
+export * from './src/bit-field';
--- packages/core/src/bit-field.ts
+++ packages/core/src/bit-field.ts
@@ -1,27 +1,45 @@
 import { Int32, ValueTypes } from "./types";
 
+type MutableProperties = "copyWithin" | "fill" | "reverse" | "set" | "sort";
+export interface ReadonlyUint32Array extends Omit<Uint32Array, MutableProperties> {
+    readonly [n: number]: number;
+}
+
 const WORD_SIZE = 32;
 
-const createBitField = (size: number, initialBitValue = false) => {
+const createBitField = (size: Int32, indexOfTrueBits?: Int32[]) => {
 
     const words = new Uint32Array(Math.ceil(size / WORD_SIZE));
-    if (initialBitValue) {
-        for (let i = 0; i < words.length; i++) {
+    if (indexOfTrueBits) {
+        for (let iWord = 0; iWord < words.length; iWord++) {
             let v = 0;
-            const bitsToSet = i === words.length - 1 ? size % 32 : 32;
+            const bitsToSet = iWord === words.length - 1 ? size % 32 : 32;
             for (let iBit = 0; iBit < bitsToSet; iBit++) {
-                v = v << 1;
+                v = v << indexOfTrueBits[iWord * WORD_SIZE + iBit];
                 v += 1;
             }
         }
-    }
+    };
 
     return {
         size,
         words,
-    };
+    } as const;
+};
+const createBitFieldReadOnly = createBitField as (size: Int32, indexOfTrueBits?: Int32[]) => {
+    readonly size: Int32,
+    readonly words: ReadonlyUint32Array,
 };
 
+const equals = (a: BitField, b: BitField): boolean => {
+    const result = createBitField(a.size);
+    for (let iWord = 0; iWord < a.words.length; iWord++) {
+        if (a.words[iWord] !== b.words[iWord]) {
+            return false;
+        }
+    }
+    return true;
+};
 const union = (a: BitField, b: BitField): BitField => {
     const result = createBitField(a.size);
     for (let iWord = 0; iWord < a.words.length; iWord++) {
@@ -29,12 +47,19 @@ const union = (a: BitField, b: BitField): BitField => {
     }
     return result;
 };
-const intersect = (a: BitField, b: BitField): BitField => {
+const intersection = (a: BitField, b: BitField): BitField => {
     const result = createBitField(a.size);
     for (let iWord = 0; iWord < a.words.length; iWord++) {
         result.words[iWord] = a.words[iWord] & b.words[iWord];
     }
     return result;
+};
+const isZero = (a: BitField): boolean => {
+    for (let iWord = 0; iWord < a.words.length; iWord++) {
+        const word = a.words[iWord];
+        if (word) { return false; }
+    }
+    return true;
 };
 const hasOneTrueBit = (a: BitField): boolean => {
     let trueBitCount = 0;
@@ -84,29 +109,52 @@ const indexOfFirstTrueBit = (a: BitField): undefined | Int32 => {
 
     return undefined;
 };
+const indexOfTrueBits = (a: BitField): Int32[] => {
+    const indexes = [] as Int32[];
+    for (let iWord = 0; iWord < a.words.length; iWord++) {
+        let word = a.words[iWord];
+        // Skip 0
+        if (!word) { continue; }
+
+        let iBitInWord = 0;
+        while (word) {
+            if (word & 1) {
+                indexes.push(ValueTypes.Int32(iWord * WORD_SIZE + iBitInWord));
+            }
+            word = word >> 1;
+            iBitInWord++;
+        }
+    }
+
+    return indexes;
+};
 const getBit = (a: BitField, i: Int32): boolean => {
     const iWord = (i / WORD_SIZE) | 0;
     const bit = i % WORD_SIZE;
     return (a.words[iWord] & (1 << bit)) !== 0;
 };
-const setBit = (a: BitField, i: Int32, value: boolean): void => {
-    const iWord = (i / WORD_SIZE) | 0;
-    const bit = i % WORD_SIZE;
-    if (value) {
-        a.words[iWord] |= 1 << bit;
-    } else {
-        a.words[iWord] &= ~(1 << bit);
-    }
-};
+// const setBit = (a: BitField, i: Int32, value: boolean): void => {
+//     const iWord = (i / WORD_SIZE) | 0;
+//     const bit = i % WORD_SIZE;
+//     if (value) {
+//         a.words[iWord] |= 1 << bit;
+//     } else {
+//         a.words[iWord] &= ~(1 << bit);
+//     }
+// };
 
-export type BitField = ReturnType<typeof createBitField>;
+export type BitField = ReturnType<typeof createBitFieldReadOnly>;
 
 export const BitField = {
-    create: createBitField,
-    intersect,
+    empty: createBitFieldReadOnly(ValueTypes.Int32(0)),
+    create: createBitFieldReadOnly,
+    equals,
     union,
+    intersection,
+    isZero,
     hasOneTrueBit,
-    indexOfTrueBit: indexOfFirstTrueBit,
+    indexOfFirstTrueBit,
+    indexOfTrueBits,
     getBit,
-    setBit,
+    // setBit,
 };
