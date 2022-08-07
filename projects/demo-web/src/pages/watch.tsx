377750bab4f128443b529cb4bfe614c1af84a228
Rick Love <rick@toldpro.com>
2022-08-07 16:00:05 -0500

    Test font size 12

--- projects/demo-web/src/pages/watch.tsx
+++ projects/demo-web/src/pages/watch.tsx
@@ -2,14 +2,14 @@ export const WatchPage = () => {
 
     return (
         <>
-            <div className='bg-black text-white w-[100vw] h-[100vh]'>
-                <div className=''>In the beginning</div>
+            <div className='bg-black text-white w-[100vw] h-[100vh] text-[12px] font-sans'>
+                <div className='mb-2'>In the beginning</div>
                 <div>
                     <div className='flex flex-row flex-wrap justify-center items-center'>
-                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>god</button>
-                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>the</button>
-                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>light</button>
-                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>first</button>
+                        <button className='m-1 p-1 min-w-[60px] min-h-[24px] bg-slate-400 active:bg-slate-600'>god</button>
+                        <button className='m-1 p-1 min-w-[60px] min-h-[24px] bg-slate-400 active:bg-slate-600'>the</button>
+                        <button className='m-1 p-1 min-w-[60px] min-h-[24px] bg-slate-400 active:bg-slate-600'>light</button>
+                        <button className='m-1 p-1 min-w-[60px] min-h-[24px] bg-slate-400 active:bg-slate-600'>first</button>
                     </div>
                 </div>
             </div>
