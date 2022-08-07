5047835045cc6daa06b6af0dc16361f8212c544c
Rick Love <rick@toldpro.com>
2022-08-07 15:48:20 -0500

    Update UI

--- projects/demo-web/src/pages/watch.tsx
+++ projects/demo-web/src/pages/watch.tsx
@@ -16,11 +16,11 @@ export const WatchPage = () => {
             <div className='bg-black text-white w-[100vw] h-[100vh]'>
                 <div className=''>In the beginning</div>
                 <div>
-                    <div className='flex flex-col leading-8'>
-                        <button>god</button>
-                        <button>the</button>
-                        <button>light</button>
-                        <button>first</button>
+                    <div className='flex flex-row flex-wrap justify-center items-center'>
+                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>god</button>
+                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>the</button>
+                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>light</button>
+                        <button className='m-1 p-1 min-w-[4rem] bg-slate-400 active:bg-slate-600'>first</button>
                     </div>
                 </div>
             </div>
