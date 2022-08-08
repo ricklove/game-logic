fe494b5d1753342d874792a3bc8af913096fb28c
Rick Love <rick@toldpro.com>
2022-08-08 10:05:29 -0500

    FIX typo

--- packages/bible-ui/src/verses.ts
+++ packages/bible-ui/src/verses.ts
@@ -47,7 +47,7 @@ const versesRaw = `
 6 “Blessed are those who hunger and thirst for righteousness, for they shall be satisfied.
 7 “Blessed are the merciful, for they shall receive mercy.
 8 “Blessed are the pure in heart, for they shall see God.
-9 “Blessed are the peacemakers, for they shall be called sons[a] of God.
+9 “Blessed are the peacemakers, for they shall be called sons of God.
 10 “Blessed are those who are persecuted for righteousness' sake, for theirs is the kingdom of heaven.
 11 “Blessed are you when others revile you and persecute you and utter all kinds of evil against you falsely on my account. 
 12 Rejoice and be glad, for your reward is great in heaven, for so they persecuted the prophets who were before you.
